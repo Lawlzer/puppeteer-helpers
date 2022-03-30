d6bb9e5058692a11b4da6e9b625af1a06df7a5be
kevindaspam@gmail.com <kevindaspam@gmail.com>
2022-03-29 21:28:49 -0500

    v1.1.0 Completely reworked all of the code
    Now, the "page" and "browser" objects have the functions
    
    - As opposed to this exposing functions that can be called.

--- index.js
+++ index.js
@@ -5,60 +5,12 @@ const Helpers = require('@lawlzer/helpers');
 const ms = require('ms');
 const fs = require('fs');
 
-async function racePromises(promises) {
-	const wrappedPromises = [];
-	promises.map((promise, index) => {
-		wrappedPromises.push(
-			new Promise((resolve) => {
-				promise
-					.then(() => {
-						resolve(index);
-					})
-					.catch();
-			}).catch()
-		);
-	});
-	return Promise.race(wrappedPromises).catch();
-}
-exports.racePromises = racePromises;
-
-const exists = async (page, selector, timeout = 100) => {
-	if (typeof page !== 'object') {
-		console.trace('In custom function Helpers.exists(page, selector, timeout), page is not an object.');
-		throw new Error('In custom function Helpers.exists(page, selector, timeout), page is not an object.');
-	}
-	try {
-		await page.waitForSelector(selector, { timeout: timeout });
-		return true;
-	} catch {
-		return false;
-	}
-};
-exports.exists = exists;
-
-const closeBrowser = async (browser) => {
-	// stops an inconsistent error
-	let pages = await browser.pages();
-	await Promise.all(pages.map((page) => page.close()));
-	await browser.close();
-};
-exports.closeBrowser = closeBrowser;
-
-const click = async (page, selector, timeout = 30000) => {
-	if (typeof page !== 'object') {
-		console.trace('Helpers.click was not passed in page as the first argument.');
-		throw new Error();
-	}
-	await page.waitForSelector(selector, { timeout: timeout });
-	await page.$eval(selector, (el) => el.click());
-};
-exports.click = click;
-
-const launchBrowser = async (realHeadless = false, fakeHeadless = false, chromePath = undefined) => {
-	var browser = await puppeteer.launch({
+// a Generator function that returns the browser and page.
+const launchBrowser = async ({ realHeadless = false, fakeHeadless = false, chromePath = undefined, slowmo = 30, viewportWidth = 1920, viewportHeight = 800 } = {}) => {
+	const browser = await puppeteer.launch({
 		executablePath: chromePath,
 		headless: realHeadless,
-		slowmo: 30,
+		slowmo: slowmo,
 		userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3419.0 Safari/537.36',
 		ignoreHTTPSErrors: true,
 		args: [
@@ -83,135 +35,121 @@ const launchBrowser = async (realHeadless = false, fakeHeadless = false, chromeP
 		// '--proxy-server=socks5://127.0.0.1:9050'] // tor if needed
 	});
 
-	let page = await browser.newPage(browser);
-	await page.setViewport({
-		width: 1920,
-		height: 800,
-	});
-	let allPages = await browser.pages();
-	await allPages[0].close(); // close the page[0] (it's about:blank)
+	await replaceBrowserFunctions(browser);
+	const page = await browser.launchPage({ viewportWidth, viewportHeight });
+
+	(await browser.pages())[0].close(); // close page[0], it's about:blank
+
 	return { browser: browser, page: page };
 };
-exports.launchBrowser = launchBrowser;
+module.exports.launchBrowser = launchBrowser;
 
-const resetBrowser = async (browser, realHeadless = false, fakeHeadless = false, chromePath = undefined) => {
-	if (browser) await closeBrowser(browser, realHeadless, fakeHeadless, chromePath); // first time this runs, we won't have a browser
-	return await launchBrowser();
+const replaceBrowserFunctions = async (browser) => {
+	// stops an inconsistent error with pages not closing automatically
+	browser.killBrowser = async () => {
+		let allPages = await browser.pages();
+		await Promise.all(allPages.map((page) => page.close()));
+		await browser.close();
+	};
+
+	browser.launchPage = async ({ viewportWidth = 1920, viewportHeight = 1080 } = {}) => {
+		const page = await browser.newPage(browser);
+		await page.setViewport({
+			width: viewportWidth,
+			height: viewportHeight,
+		});
+		await replacePageFunctions(page);
+		return page;
+	};
+
+	browser.resetBrowser = async () => {
+		const page = await browser.launchPage();
+		const client = await page.target().createCDPSession();
+		await client.send('Network.clearBrowserCookies');
+		await client.send('Network.clearBrowserCache');
+		await page.close();
+		// await browser.killBrowser();
+		// return await launchBrowser();
+	};
 };
-exports.resetBrowser = resetBrowser;
 
-const waitForAnySelector = async (page, selectors, timeout = 30000, trace = true) => {
-	if (typeof page !== 'object') {
-		console.trace('in waitForAnySelector, page was not passed in as an object. Throwing.');
-		throw new Error();
-	}
+const replacePageFunctions = async (page) => {
+	page.originalClick = page.click;
+	page.click = async (selector, timeout = 30000) => {
+		await page.waitForSelector(selector, { timeout: timeout });
+		await page.$eval(selector, (el) => el.click());
+	};
 
-	const startTime = Date.now();
+	page.originalType = page.type;
+	page.type = async (selector, text) => {
+		await page.waitForSelector(selector);
+		await page.originalType(selector, text);
+	};
 
-	while (true) {
-		await sleep(ms('0.1s'));
-		if (Date.now() - startTime > timeout) {
-			if (trace) console.trace('None of the possibilities showed up in waitForAnySelector. Returning null. Input selectors: ', selectors);
-			return null;
+	page.exists = async (selector, timeout = 50) => {
+		try {
+			await page.waitForSelector(selector, { timeout: timeout });
+			return true;
+		} catch {
+			return false;
 		}
+	};
 
-		for await (selector of selectors) {
-			if (await exists(page, selector, 100)) {
-				return selector;
+	page.waitForAnyOfSelectors = async (selectors, timeout = 30000) => {
+		const startTime = Date.now();
+		while (Date.now() - startTime < timeout) {
+			await sleep(ms('0.01s'));
+
+			for await (selector of selectors) {
+				if (await exists(page, selector, 100)) return selector;
 			}
 		}
-	}
-};
-exports.waitForAnySelector = waitForAnySelector;
+		return null;
+	};
 
-const type = async (page, selector, text) => {
-	if (typeof page !== 'object') {
-		console.trace('in Helpers.type, order should be page(type, selector, text). page was not an object.');
-		throw new Error();
-	}
-	if (typeof selector !== 'string') {
-		console.trace('in Helpers.type, order should be page(type, selector, text). text was not a string.');
-		throw new Error();
-	}
-	if (typeof text !== 'string') {
-		console.trace('in Helpers.type, order should be page(type, selector, text). text was not a strings.');
-		throw new Error();
-	}
-	await page.waitForSelector(selector);
-	await page.type(selector, text);
-};
-exports.type = type;
-
-const waitForNumberInnerHTML = async (page, selector, timeout = 30000) => {
-	const start = Date.now();
-	while (Date.now() - start < timeout) {
-		await sleep(ms('0.1s')); // sleep a tiny bit
-		let maybeNumber = await page.$eval(selector, (el) => el.innerHTML);
-		maybeNumber = parseInt(await replaceAll(maybeNumber, ',', '')); // remove all commas
-		if (typeof maybeNumber === 'number' && !isNaN(maybeNumber)) return maybeNumber;
-	}
-
-	console.trace('Error in waitForNumber: waited for over 30 seconds, did not get a number. Throwing error. Selector: ', selector);
-	throw new Error();
-};
-exports.waitForNumberInnerHTML = waitForNumberInnerHTML;
-
-const saveErrorScreenshot = async (page, error, extraInfo = undefined) => {
-	if (!fs.existsSync(screenshotPath)) fs.mkdirSync(screenshotPath, { recursive: true });
-	let existingScreenshots = fs.readdirSync(screenshotPath);
-	existingScreenshots = existingScreenshots.sort((b, a) => parseInt(a) - parseInt(b));
-	let fileName = '0';
-	if (existingScreenshots.length > 0) fileName = parseInt(existingScreenshots[0]) + 1;
-	await page.screenshot({
-		path: screenshotPath + '/' + fileName + '.png',
-		fullPage: true,
-	});
-	let message = error.stack;
-	if (extraInfo) message += '\nExtra passed in info: `' + extraInfo + '`';
-	fs.writeFileSync(screenshotPath + '/' + fileName + '.txt', message);
-};
-exports.saveErrorScreenshot = saveErrorScreenshot;
-
-const waitForChildNodeToChange = async (page, selector, timeout = 30000) => {
-	const start = Date.now();
-	await page.waitForSelector(selector);
-	let initialChildNodeAmount = await page.$eval(selector, (el) => el.childNodes.length);
-	while (Date.now() - start < timeout) {
-		await sleep(ms('0.01s')); // sleep a tiny bit
-		let currentChildNodeAmount = await page.$eval(selector, (el) => el.childNodes.length);
-		// if (typeof maybeNumber === 'number' && !isNaN(maybeNumber)) return maybeNumber;
-		if (currentChildNodeAmount !== initialChildNodeAmount) {
-			return currentChildNodeAmount;
+	page.clickUntilSelectorAppears = async (selector, waitForThis, timeout = 30000) => {
+		const start = Date.now();
+		while (Date.now() - start < timeout) {
+			await sleep(ms('0.01s'));
+			if (await exists(page, waitForThis, 10)) return true;
+			await click(page, selector);
 		}
-	}
+		throw new Error('page.clickUntilSelectorAppears: we never got selector: ' + waitForThis);
+	};
 
-	console.trace('Error in waitForChildNodeToChange: waited for over 30 seconds, did not change. Throwing error. Selector: ', selector);
-	throw new Error('Error in waitForChildNodeToChange: waited for over 30 seconds, did not change. Throwing error. Selector: ', selector);
+	page.clickUntilSelectorDisappears = async (selector, timeout = 30000) => {
+		const start = Date.now();
+		await page.waitForSelector(selector);
+		while (Date.now() - start < timeout) {
+			await sleep(ms('0.01s')); // sleep a tiny bit
+			if (!(await exists(page, selector, 10))) return;
+			await click(page, selector);
+		}
+		throw new Error('page.clickUntilSelectorDisappears: Never saw selector: : ' + selector);
+	};
+
+	page.waitForChildNodeAmountToChange = async (selector, timeout = 30000) => {
+		const start = Date.now();
+		await page.waitForSelector(selector);
+		let initialChildNodeAmount = await page.$eval(selector, (el) => el.childNodes.length);
+		while (Date.now() - start < timeout) {
+			await sleep(ms('0.01s')); // sleep a tiny bit
+			let currentChildNodeAmount = await page.$eval(selector, (el) => el.childNodes.length);
+			if (currentChildNodeAmount !== initialChildNodeAmount) {
+				return currentChildNodeAmount;
+			}
+		}
+		throw new Error('page.waitForChildNodeAmountToChange: waited for timeout, but it did not change. Throwing error. Selector: ', selector);
+	};
+
+	page.waitForNumberInnerHTML = async (selector, timeout = 30000) => {
+		const start = Date.now();
+		while (Date.now() - start < timeout) {
+			await sleep(ms('0.01s')); // sleep a tiny bit
+			let maybeNumber = await page.$eval(selector, (el) => el.innerHTML);
+			maybeNumber = parseInt(await replaceAll(maybeNumber, ',', '')); // remove all commas
+			if (typeof maybeNumber === 'number' && !isNaN(maybeNumber)) return maybeNumber;
+		}
+		throw new Error('page.waitForNumberInnerHTML: waited for over 30 seconds, did not get a number. Throwing error. Selector: ' + selector);
+	};
 };
-exports.waitForChildNodeToChange = waitForChildNodeToChange;
-
-const clickUntilSelectorAppears = async (page, selector, waitForThis, timeout = 30000) => {
-	const start = Date.now();
-
-	while (Date.now() - start < timeout) {
-		await sleep(ms('0.05s'));
-		if (await exists(page, waitForThis, 10)) return;
-		await click(page, selector);
-	}
-	console.trace('in clickUntilSelectorAppears, we never got selector: ', waitForThis);
-	throw new Error('in clickUntilSelectorAppears, we never got selector: ' + waitForThis);
-};
-exports.clickUntilSelectorAppears = clickUntilSelectorAppears;
-
-const clickUntilSelectorDisappears = async (page, selector, timeout = 30000) => {
-	const start = Date.now();
-	await page.waitForSelector(selector);
-	while (Date.now() - start < timeout) {
-		await sleep(ms('0.01s')); // sleep a tiny bit
-		if (!(await exists(page, selector, 10))) return;
-		await click(page, selector);
-	}
-	console.trace('Selector never appeared in clickUntilSelectorDisappears: ' + selector);
-	throw new Error('Selector never appeared in clickUntilSelectorDisappears: ' + selector);
-};
-exports.clickUntilSelectorDisappears = clickUntilSelectorDisappears;
